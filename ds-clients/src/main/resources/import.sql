d1f675fec5d5e04c04571735e43785ce52bf06b8
pedro amaral <amaralph1995@gmail.com>
2024-12-11 17:26:15 -0300

    create seeding

---
+++ ds-clients/src/main/resources/import.sql
@@ -0,0 +1,29 @@
+-- Cliente 1
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ana Souza', '123.456.789-00', 3500.00, '1985-06-15', 2);
+
+-- Cliente 2
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos Silva', '234.567.890-11', 5500.00, '1990-02-20', 1);
+
+-- Cliente 3
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Maria Oliveira', '345.678.901-22', 4500.00, '1982-11-05', 3);
+
+-- Cliente 4
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('João Pereira', '456.789.012-33', 6000.00, '1980-01-30', 0);
+
+-- Cliente 5
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Juliana Costa', '567.890.123-44', 8000.00, '1995-07-25', 2);
+
+-- Cliente 6
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ricardo Santos', '678.901.234-55', 12000.00, '1978-09-10', 4);
+
+-- Cliente 7
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fernanda Lima', '789.012.345-66', 4000.00, '1992-04-18', 1);
+
+-- Cliente 8
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lucas Martins', '890.123.456-77', 7000.00, '1988-12-12', 0);
+
+-- Cliente 9
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Patrícia Souza', '901.234.567-88', 5500.00, '1993-08-30', 2);
+
+-- Cliente 10
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Felipe Rocha', '012.345.678-99', 9500.00, '1987-03-22', 3);
