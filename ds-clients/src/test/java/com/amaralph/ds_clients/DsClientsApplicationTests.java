1325dd3441a631e39ea3ec601814d0a5d372da27
pedro amaral <amaralph1995@gmail.com>
2024-12-11 16:53:57 -0300

    initial commit

---
+++ ds-clients/src/test/java/com/amaralph/ds_clients/DsClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amaralph.ds_clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
