25c25ac8dd444e6cc1efc1debfc7081853c86d09
pedro amaral <amaralph1995@gmail.com>
2024-12-11 19:30:26 -0300

    create findById service and controller

--- ds-clients/src/main/java/com/amaralph/ds_clients/services/ClientService.java
+++ ds-clients/src/main/java/com/amaralph/ds_clients/services/ClientService.java
@@ -3,6 +3,7 @@ package com.amaralph.ds_clients.services;
 import com.amaralph.ds_clients.dto.ClientDTO;
 import com.amaralph.ds_clients.entities.Client;
 import com.amaralph.ds_clients.repositories.ClientRepository;
+import com.amaralph.ds_clients.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,5 +18,11 @@ public class ClientService {
     public Page<ClientDTO> findAll(Pageable pageable) {
         Page<Client> result = clientRepository.findAll(pageable);
         return result.map(x -> new ClientDTO(x));
+    }
+
+    public ClientDTO findById(Long id) {
+        Client client = clientRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado"));
+        return new ClientDTO(client);
     }
 }
